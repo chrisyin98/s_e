681e6ef5994f8fba36a88640b063b29c690c2fab
chrisyin98 <chrisyin98@gmail.com>
2020-07-15 14:24:25 -0700

    newest update

--- main.py
+++ main.py
@@ -14,25 +14,21 @@ class Search_Engine:
     first = self.first_index_find(index, left, right, term, websites)
     last = self.last_index_find(index, left, right, term, websites)
 
-    amt_of_found = (last + 1) - first
-
     for a in range(first,last+1):
       display_terms.append(websites[a])
 
-    frequency = len(display_terms) / amt_of_found
-    inverse_frequency = math.log(len(websites)/len(display_terms))
-    tfidf = frequency * inverse_frequency
-    display_terms = sorted(display_terms)
-    self.auto_c_results[tfidf] = display_terms
-    print(self.auto_c_results)
+
+    display_terms = sorted(display_terms, key= lambda x: x.get_weight())
+    #self.auto_c_results[tfidf] = display_terms
+    #print(self.auto_c_results)
     return display_terms
 
   def first_index_find(self, index, left, right, term, websites):
     while(left <= right):
       mid = int(left + ((right - left) / 2))
-      index_word = websites[mid]
+      index_word = websites[mid].get_term()
       if (len(term) < len(index_word)):
-        index_word = websites[mid][0:len(term)]
+        index_word = websites[mid].get_term()[0:len(term)]
       if(index_word >= term):
         right = mid - 1
       else:
@@ -44,9 +40,9 @@ class Search_Engine:
   def last_index_find(self, index, left, right, term, websites):
     while(left <= right):
       mid = int(left + ((right - left) / 2))
-      index_word = websites[mid]
+      index_word = websites[mid].get_term()
       if (len(term) < len(index_word)):
-        index_word = websites[mid][0:len(term)]
+        index_word = websites[mid].get_term()[0:len(term)]
       if(index_word <= term):
         left = mid + 1
       else:
@@ -57,29 +53,44 @@ class Search_Engine:
 
 
 def main():
-  df = pd.read_csv('./dataset/small.csv')
-  traffic = df.Avg_Daily_Visitors.tolist()[1:]
-  new_list = [x.lower() for x in df.Website.tolist()[1:]]
-  websites_list = sorted(new_list)
-  print(websites_list)
+  df = pd.read_csv('C:/Users/Christopher/Desktop/python/search_engine/dataset/small.csv')
+  traffic = df.Avg_Daily_Visitors.tolist() #df.Avg_Daily_Visitors.tolist()[1:]
+  total_avg_visitors = df.Avg_Daily_Visitors.sum() / 100000
+  new_list = [x.lower() for x in df.Website.tolist()]  # in df.Website.tolist()[1:]
+  websites_prep = sorted(new_list)
+  websites_list = []
+  for x in range(len(websites_prep)):
+    freq_prep = (traffic[x]) / 100000
+    frequency = freq_prep / total_avg_visitors
+    inverse_frequency = math.log(total_avg_visitors / freq_prep)
+    tfidf = frequency * inverse_frequency
+    websites_list.append(Term(websites_prep[x], tfidf))
+
+
   s_e = Search_Engine()
-  search_query = s_e.search_algorithm("www.g", websites_list)
-  print(search_query)
+#  user_input = ""
+#while(user_input != '$'):
+# user_input = input('Search?')
+  search_query = s_e.search_algorithm("www.go", websites_list)
+  for x in range(len(search_query)):
+    print(search_query[x].get_term())
+
 
 
 class Term:
-  def __init__(self, weight, term):
+  def __init__(self,term, weight):
     self.weight = weight
     self.term = term
 
-  def weight(self):
+  def get_weight(self):
     return self.weight
 
-  def term(self):
+  def get_term(self):
     return self.term
 
   def add_weight(self):
-    self.weight ==
+    self.weight = self.weight + 1
+
 
 
 main()
