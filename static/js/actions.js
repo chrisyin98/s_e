eddfde14faa6b8ef3e2fd8f344933e7381bfee59
chrisyin98 <chrisyin98@gmail.com>
2020-07-25 18:17:06 -0700

    updated look ;

--- static/js/actions.js
+++ static/js/actions.js
@@ -24,15 +24,19 @@ function displayResults(info) {
   id('s-results').innerHTML = "";
   if(info.length < 1) {
     let text = document.createElement('p');
-    text.textContent = "No results found"
+    text.textContent = "No results found."
     id('s-results').appendChild(text);
   }
   for(let i = 0; i < info.length; i++) {
     let text = document.createElement('p');
     text.textContent = "Website: " + info[i].website_nme;
+    let num = document.createElement('p');
+    num.textContent = "cos_sim weight: " + info[i].weight;
+    num.style = "font-size:10px;";
     let text2 = document.createElement('p');
     text2.textContent = "Description: " + info[i].descrip;
     id('s-results').appendChild(text);
+    id('s-results').appendChild(num)
     id('s-results').appendChild(text2);
   }
 
